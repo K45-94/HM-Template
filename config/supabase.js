60852192f01cde35a25e16832daab81bfd4b70ca
K45-94 <kelvinhiuhu@live.com>
2023-03-07 01:06:14 +0300

    message

---
+++ config/supabase.js
@@ -0,0 +1,7 @@
+export default {
+  credentials: {
+    supabaseUrl: "https://vcvfhtibxdgbxmlvhpcw.supabase.co",
+    supabaseKey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjdmZodGlieGRnYnhtbHZocGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxMTEzMDMsImV4cCI6MTk5MzY4NzMwM30.7eP-ej9lHHmPqHrSKYC0VR2wVRKUh9C-A97ZK8xTp9g",
+  },
+};
---
+++ src/auth/boot/registerAuthRoutes.js
@@ -0,0 +1,45 @@
+import { boot } from "quasar/wrappers";
+
+export default boot(({ router }) => {
+  router.addRoute("/", {
+    name: "auth.register",
+    path: "/register",
+    meta: { unauthOnly: true },
+    component: () => import("src/auth/pages/IdentityPasswordRegisterPage.vue"),
+  });
+
+  router.addRoute("/", {
+    name: "auth.login",
+    path: "/login",
+    meta: { unauthOnly: true },
+    component: () => import("src/auth/pages/IdentityPasswordLoginPage.vue"),
+  });
+
+  router.addRoute("/", {
+    name: "auth.requestPasswordReset",
+    path: "/forgot-password",
+    meta: { unauthOnly: true },
+    component: () =>
+      import("src/auth/pages/PasswordResetRequestViaEmailPage.vue"),
+  });
+
+  router.addRoute("/", {
+    name: "auth.resetPassword",
+    path: "/password-reset",
+    meta: { unauthOnly: true },
+    component: () => import("src/auth/pages/PasswordResetViaEmailPage.vue"),
+  });
+
+  router.addRoute("/", {
+    path: "/feeds",
+    component: () => import("layouts/PlumLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "PageFeeds",
+        component: () => import("src/pages/PageFeeds.vue"),
+        meta: { authOnly: true },
+      },
+    ],
+  });
+});
